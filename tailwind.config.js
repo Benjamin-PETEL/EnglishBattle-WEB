0214e1088856effce91528f6d639876ef1efcea1
Benjamin PETEL <benjamin.petel@outlook.com>
2022-05-12 16:01:19 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class', 
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'current',
+        dark: '#0A0001',
+        grey: '#381E21',
+        main: '#640211',
+        pink: '#F8D4DA',
+        white: '#FFF6F7'
+    }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
