439099067b29742591470ad58b1ffa79f6c5f6b5
PETEL <benjamin.petel@capgemini.com>
2022-07-20 18:04:54 +0200

    Clean: remove simple component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { VerbsComponent } from './components/verbs/verbs.component';
 import { VerbService } from './services/verb.service';
-import { SimpleService } from './services/simple.service';
-import { SimpleComponent } from './components/simple/simple.component';
 
 @NgModule({
   declarations: [
